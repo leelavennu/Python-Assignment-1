116af1941b104e709de6d643f9efea4a4001afab
leelavennu <leelavennu@gmail.com>
2026-01-02 17:00:13 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+s1= float(input("Enter the first number: "))
+s2=float(input("Enter the second number: "))
+
+Addition= s1+s2
+Subtraction=s1-s2
+Multiplication=s1*s2
+Divison=s1/s2
+print("Addition:",Addition)
+print("Subtraction:",Subtraction)
+print("Multiplication:",Multiplication)
+print("Divison:",Divison)
---
+++ task2.py
@@ -0,0 +1,4 @@
+s1= input("Enter your first name: ")
+s2=input("Enter your last name: ")
+s3=s1+" "+s2+"!"
+print("Hello,",s3,"Welcome to the Python program")
